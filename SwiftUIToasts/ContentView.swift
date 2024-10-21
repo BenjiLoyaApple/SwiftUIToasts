15c1074c8ac12cca706192334d73b31ec49ba67c
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2024-10-21 13:55:45 +0500

    Initial Commit

---
+++ SwiftUIToasts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIToasts
+//
+//  Created by Benji Loya on 21.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIToasts/SwiftUIToastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToastsApp.swift
+//  SwiftUIToasts
+//
+//  Created by Benji Loya on 21.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
