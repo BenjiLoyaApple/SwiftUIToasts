1d335e0e27f8ff07caaf1cb0a23f784368f604bb
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2024-10-21 16:18:09 +0500

    v1 Complete

--- SwiftUIToasts/ContentView.swift
+++ SwiftUIToasts/ContentView.swift
@@ -8,15 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var toasts: [Toast] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Text("Dummy List Row View")
+            }
+            .navigationTitle("Toasts")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Show", action: showToast)
+                }
+            }
         }
-        .padding()
+        .interactiveToasts($toasts)
     }
+    
+    func showToast() {
+        withAnimation(.bouncy) {
+            let toast = Toast { id in
+                ToastView(id)
+            }
+            
+            toasts.append(toast)
+        }
+    }
+    
+    /// CUSTOM TOAST VIEW
+    @ViewBuilder
+    func ToastView(_ id: String) -> some View {
+        HStack(spacing: 12) {
+            Image(systemName: "mic")
+            
+            Text("Hello ...")
+                .font(.callout)
+            
+            Spacer(minLength: 0)
+            
+            Button {
+                $toasts.delete(id)
+            } label: {
+                Image(systemName: "xmark.circle.fill")
+                    .font(.title2)
+            }
+            
+        }
+        .foregroundStyle(Color.primary)
+        .padding(.vertical, 12)
+        .padding(.leading, 15)
+        .padding(.trailing, 10)
+        .background {
+            Capsule()
+                .fill(.background)
+                .shadow(color: .black.opacity(0.06), radius: 3, x: -1, y: -3)
+                .shadow(color: .black.opacity(0.06), radius: 2, x: 1, y: 3)
+        }
+        .padding(.horizontal, 15)
+        
+    }
+    
 }
 
 #Preview {
